e1328e8efa3e8bb88eaed32c7f1870c5182df82f
Alexis Lozano <alexis.pascal.lozano@gmail.com>
2021-05-04 01:05:42 +0200

    Chapter 12: Where next?

--- main.go
+++ main.go
@@ -14,6 +14,87 @@ import (
 	"github.com/alexislozano/go-raytracing/vec3"
 )
 
+func randomScene() hitable.Hitable {
+	list := []hitable.Hitable{}
+
+	list = append(list, &hitable.Sphere{
+		Center:   vec3.Vec3{X: 0, Y: -1000, Z: 0},
+		Radius:   1000,
+		Material: &material.Lambertian{Albedo: vec3.Vec3{X: 0.5, Y: 0.5, Z: 0.5}},
+	})
+
+	for a := -11; a < 11; a++ {
+		for b := -11; b < 11; b++ {
+			chooseMat := rand.Float64()
+			center := vec3.Vec3{
+				X: float64(a) + 0.9*rand.Float64(),
+				Y: 0.2,
+				Z: float64(b) + 0.9*rand.Float64(),
+			}
+
+			if vec3.Sub(center, vec3.Vec3{X: 4, Y: 0.2, Z: 0}).Length() > 0.9 {
+				if chooseMat < 0.8 {
+					list = append(list, &hitable.Sphere{
+						Center: center,
+						Radius: 0.2,
+						Material: &material.Lambertian{
+							Albedo: vec3.Vec3{
+								X: rand.Float64() * rand.Float64(),
+								Y: rand.Float64() * rand.Float64(),
+								Z: rand.Float64() * rand.Float64(),
+							},
+						},
+					})
+				} else if chooseMat < 0.95 {
+					list = append(list, &hitable.Sphere{
+						Center: center,
+						Radius: 0.2,
+						Material: &material.Metal{
+							Albedo: vec3.Vec3{
+								X: 0.5 * (1 + rand.Float64()),
+								Y: 0.5 * (1 + rand.Float64()),
+								Z: 0.5 * (1 + rand.Float64()),
+							},
+							Fuzz: 0.5 * rand.Float64(),
+						},
+					})
+				} else {
+					list = append(list, &hitable.Sphere{
+						Center:   center,
+						Radius:   0.2,
+						Material: &material.Dielectric{RefIdx: 1.5},
+					})
+				}
+			}
+		}
+	}
+
+	list = append(list, &hitable.Sphere{
+		Center:   vec3.Vec3{X: 0, Y: 1, Z: 0},
+		Radius:   1,
+		Material: &material.Dielectric{RefIdx: 1.5},
+	})
+
+	list = append(list, &hitable.Sphere{
+		Center: vec3.Vec3{X: -4, Y: 1, Z: 0},
+		Radius: 1,
+		Material: &material.Lambertian{
+			Albedo: vec3.Vec3{X: 0.4, Y: 0.2, Z: 0.1},
+		},
+	})
+
+	list = append(list, &hitable.Sphere{
+		Center: vec3.Vec3{X: 4, Y: 1, Z: 0},
+		Radius: 1,
+		Material: &material.Metal{
+			Albedo: vec3.Vec3{X: 0.7, Y: 0.6, Z: 0.5},
+			Fuzz:   0,
+		},
+	})
+
+	return &hitable.HitableList{List: list}
+}
+
 func color(r *ray.Ray, world hitable.Hitable, depth int) vec3.Vec3 {
 	hit, rec, mat := world.Hit(r, 0.001, math.MaxFloat64)
 	if hit {
@@ -42,42 +123,12 @@ func main() {
 	var image strings.Builder
 	image.WriteString(fmt.Sprintf("P3\n%d %d\n255\n", imageWidth, imageHeight))
 
-	list := []hitable.Hitable{
-		&hitable.Sphere{
-			Center:   vec3.Vec3{X: 0.0, Y: 0.0, Z: -1.0},
-			Radius:   0.5,
-			Material: &material.Lambertian{Albedo: vec3.Vec3{X: 0.8, Y: 0.3, Z: 0.3}},
-		},
-		&hitable.Sphere{
-			Center:   vec3.Vec3{X: 0.0, Y: -100.5, Z: -1.0},
-			Radius:   100,
-			Material: &material.Lambertian{Albedo: vec3.Vec3{X: 0.8, Y: 0.8, Z: 0.0}},
-		},
-		&hitable.Sphere{
-			Center: vec3.Vec3{X: 1, Y: 0, Z: -1},
-			Radius: 0.5,
-			Material: &material.Metal{
-				Albedo: vec3.Vec3{X: 0.8, Y: 0.6, Z: 0.2},
-				Fuzz:   0.3,
-			},
-		},
-		&hitable.Sphere{
-			Center:   vec3.Vec3{X: -1, Y: 0, Z: -1},
-			Radius:   0.5,
-			Material: &material.Dielectric{RefIdx: 1.5},
-		},
-		&hitable.Sphere{
-			Center:   vec3.Vec3{X: -1, Y: 0, Z: -1},
-			Radius:   -0.45,
-			Material: &material.Dielectric{RefIdx: 1.5},
-		},
-	}
-	world := hitable.HitableList{List: list}
+	world := randomScene()
 
-	lookFrom := vec3.Vec3{X: 3, Y: 3, Z: 2}
-	lookAt := vec3.Vec3{X: 0, Y: 0, Z: -1}
+	lookFrom := vec3.Vec3{X: 16, Y: 2, Z: 4}
+	lookAt := vec3.Vec3{X: 0, Y: 0, Z: 0}
 	distToFocus := vec3.Sub(lookFrom, lookAt).Length()
-	aperture := 2.0
+	aperture := 0.2
 
 	cam := camera.New(
 		lookFrom,
@@ -96,7 +147,7 @@ func main() {
 				u := (float64(i) + rand.Float64()) / imageWidth
 				v := (float64(j) + rand.Float64()) / imageHeight
 				r := cam.GetRay(u, v)
-				col = vec3.Add(col, color(&r, &world, 0.0))
+				col = vec3.Add(col, color(&r, world, 0.0))
 			}
 
 			col = vec3.DivCoeff(col, raysPerPixel)
